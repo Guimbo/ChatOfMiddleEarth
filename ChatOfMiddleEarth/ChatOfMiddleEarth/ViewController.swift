2468f971c06a8592158bb011c1518e3fac596459
Guilherme <guiaszul@gmail.com>
2020-08-04 20:04:54 -0300

    Creating Xcode Project

---
+++ ChatOfMiddleEarth/ChatOfMiddleEarth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatOfMiddleEarth
+//
+//  Created by Guilherme Araujo on 04/08/20.
+//  Copyright © 2020 Guilherme Araujo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
