92bfa5d1488de872b11a01bd4163bae3183db98f
Guilherme <guiaszul@gmail.com>
2020-08-08 20:58:16 -0300

    Add interface in LoginViewController

--- ChatOfMiddleEarth/ChatOfMiddleEarth/Screens/Login/LoginViewController.swift
+++ ChatOfMiddleEarth/ChatOfMiddleEarth/Screens/Login/LoginViewController.swift
@@ -8,10 +8,25 @@
 
 import UIKit
 
+protocol LoginViewPresenting {
+    
+    func joinInChat()
+}
+
 class LoginViewController: UIViewController {
     
-    let loginView = LoginView()
-
+    private lazy var loginView = LoginView()
+    private let presenter: LoginViewPresenting?
+    
+    init(presenter: LoginViewPresenting) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
