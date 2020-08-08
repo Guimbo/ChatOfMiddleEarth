9807185fe9e780406936d21939364b9464c28c49
Guilherme <guiaszul@gmail.com>
2020-08-08 20:27:17 -0300

    Add LoginView

---
+++ ChatOfMiddleEarth/ChatOfMiddleEarth/Screens/Login/View/LoginView.swift
@@ -0,0 +1,31 @@
+//
+//  LoginView.swift
+//  ChatOfMiddleEarth
+//
+//  Created by Guilherme Araujo on 08/08/20.
+//  Copyright © 2020 Guilherme Araujo. All rights reserved.
+//
+
+import UIKit
+
+class LoginView: UIView {
+    
+    @IBOutlet weak var contentView: UIView!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        Bundle.main.loadNibNamed("LoginView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+}
