d7954bd23517528d46dca0cfbf5e115476b83275
Guilherme <guiaszul@gmail.com>
2020-08-08 20:33:57 -0300

    Add ViewController and Presenter

---
+++ ChatOfMiddleEarth/ChatOfMiddleEarth/Screens/Login/LoginPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  LoginPresenter.swift
+//  ChatOfMiddleEarth
+//
+//  Created by Guilherme Araujo on 08/08/20.
+//  Copyright © 2020 Guilherme Araujo. All rights reserved.
+//
+
+import Foundation
---
+++ ChatOfMiddleEarth/ChatOfMiddleEarth/Screens/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginViewController.swift
+//  ChatOfMiddleEarth
+//
+//  Created by Guilherme Araujo on 08/08/20.
+//  Copyright © 2020 Guilherme Araujo. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
