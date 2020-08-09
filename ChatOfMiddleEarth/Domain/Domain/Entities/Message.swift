3541f14e96b400e97c1be2eb1bc3b706ddcc9099
Guilherme <guiaszul@gmail.com>
2020-08-09 01:39:30 -0300

    add Message Struct

--- ChatOfMiddleEarth/Domain/Domain/Entities/Message.swift
+++ ChatOfMiddleEarth/Domain/Domain/Entities/Message.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+struct Message {
+  let message: String
+  let senderUsername: String
+  let messageSender: MessageSender
+  
+  init(message: String, messageSender: MessageSender, username: String) {
+    self.message = message.withoutWhitespace()
+    self.messageSender = messageSender
+    self.senderUsername = username
+  }
+}
