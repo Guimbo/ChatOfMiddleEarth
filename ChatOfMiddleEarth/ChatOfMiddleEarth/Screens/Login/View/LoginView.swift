81b3d3366de42fe635e844f5cb9efc728c41a169
Guilherme <guiaszul@gmail.com>
2020-08-09 01:05:41 -0300

    setup TExtFields

--- ChatOfMiddleEarth/ChatOfMiddleEarth/Screens/Login/View/LoginView.swift
+++ ChatOfMiddleEarth/ChatOfMiddleEarth/Screens/Login/View/LoginView.swift
@@ -8,9 +8,35 @@
 
 import UIKit
 
+protocol LoginViewDelegate: AnyObject {
+    func doLogin(withUser user: String, andPort port: String)
+}
+
 class LoginView: UIView {
     
+    
+    //Outlets
     @IBOutlet var contentView: UIView!
+    @IBOutlet weak var userNameTextField: UITextField! {
+        didSet {
+            userNameTextField.keyboardType = .default
+            userNameTextField.returnKeyType = .done
+            userNameTextField.autocorrectionType = .no
+        }
+    }
+    @IBOutlet weak var portNumberTextField: UITextField!{
+        didSet {
+            portNumberTextField.keyboardType = .numberPad
+            portNumberTextField.returnKeyType = .done
+            portNumberTextField.autocorrectionType = .no
+        }
+    }
+    
+    
+    
+    //Properties
+    weak var delegate: LoginViewDelegate?
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -21,7 +47,16 @@ class LoginView: UIView {
         super.init(coder: coder)
         commonInit()
     }
+
+    @IBAction func doLogin(_ sender: Any) {
+        delegate?.doLogin(withUser: userNameTextField.text ?? "",
+                          andPort: portNumberTextField.text ?? "")
+    }
     
+}
+
+extension LoginView {
+
     private func commonInit() {
         Bundle.main.loadNibNamed("LoginView", owner: self, options: nil)
         addSubview(contentView)
