c0decf055b0a9937b59ce10aab73c3e489f3d42e
Guilherme <guiaszul@gmail.com>
2020-08-09 01:49:13 -0300

    thinking in JoinChatUseCase business logic

---
+++ ChatOfMiddleEarth/Domain/Domain/UseCases/JoinChatUseCase.swift
@@ -0,0 +1,14 @@
+//
+//  JoinChatUseCase.swift
+//  Domain
+//
+//  Created by Guilherme Araujo on 09/08/20.
+//  Copyright © 2020 Guilherme Araujo. All rights reserved.
+//
+
+import Foundation
+
+// Instanciar um chat Shared
+// passar para a proxima view o nome e para o chatRoom o port number em UInt32
+// tentará dá um join chat, se não conseguir mostra warning
+// se conseguir abre next tela
