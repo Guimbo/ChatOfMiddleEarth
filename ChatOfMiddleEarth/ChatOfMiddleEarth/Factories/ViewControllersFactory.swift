5596714b02637662a04d0eba87dcdf771e6e4513
Guilherme <guiaszul@gmail.com>
2020-08-08 21:17:37 -0300

    Creating ViewControllerFactory and add Login

---
+++ ChatOfMiddleEarth/ChatOfMiddleEarth/Factories/ViewControllersFactory.swift
@@ -0,0 +1,25 @@
+//
+//  ViewControllerFactory.swift
+//  ChatOfMiddleEarth
+//
+//  Created by Guilherme Araujo on 08/08/20.
+//  Copyright © 2020 Guilherme Araujo. All rights reserved.
+//
+
+import Foundation
+
+protocol ViewControllersFactory {
+    func makeLoginViewController() -> LoginViewController
+}
+
+class ViewControllersFactoryImplementation: ViewControllersFactory {
+    
+    let coordinator = MainCoordinator()
+    
+    func makeLoginViewController() -> LoginViewController {
+        let presenter = LoginPresenter(coordinator: coordinator)
+        let loginVC = LoginViewController(presenter: presenter)
+
+        return loginVC
+    }
+}
