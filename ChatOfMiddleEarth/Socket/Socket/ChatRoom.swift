88cece10f4573c19d88f06f8233b465973aca30b
Guilherme <guiaszul@gmail.com>
2020-08-09 01:58:58 -0300

    Add Socket Layer to project with Chat Room

---
+++ ChatOfMiddleEarth/Socket/Socket/ChatRoom.swift
@@ -0,0 +1,9 @@
+//
+//  ChatRoomImplementation.swift
+//  Socket
+//
+//  Created by Guilherme Araujo on 09/08/20.
+//  Copyright © 2020 Guilherme Araujo. All rights reserved.
+//
+
+import Foundation
---
+++ ChatOfMiddleEarth/Socket/Socket/Socket.h
@@ -0,0 +1,19 @@
+//
+//  Socket.h
+//  Socket
+//
+//  Created by Guilherme Araujo on 09/08/20.
+//  Copyright © 2020 Guilherme Araujo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Socket.
+FOUNDATION_EXPORT double SocketVersionNumber;
+
+//! Project version string for Socket.
+FOUNDATION_EXPORT const unsigned char SocketVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Socket/PublicHeader.h>
+
+
